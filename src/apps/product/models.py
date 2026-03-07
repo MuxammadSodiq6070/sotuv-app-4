8d04fc6fa2f2ee1f329a0c1395e438c7e8201728
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-03-07 20:30:54 +0500

    base.html va dashboard.html va login.html tayyorlandi

--- src/apps/product/models.py
+++ src/apps/product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from apps.shop.models import Shop
+
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    products_count = models.DecimalField(max_digits=10, decimal_places=0, default=0)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.Image 
+
+     
--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Stuff , User
 # Register your models here.
+
+
+admin.site.register(Stuff)
+admin.site.register(User)
