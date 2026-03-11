49b9e3724e84533ef181fbcb082a2cafbbb8e614
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-03-11 14:32:15 +0500

    Hatolarni tuzatdim

--- src/config/settings.py
+++ src/config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = ['vinihew105devlugcom.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
