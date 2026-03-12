c4f9ebf546e0ca6d807fe61f3188f33edd3a8e46
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-03-12 20:06:44 +0500

    yana hech narsa

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,7 +5,7 @@ import environ
 env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(file).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
@@ -17,9 +17,21 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env("DEBUG"))
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
+import os
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "vinihew105devlugcom.pythonanywhere.com",
+]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    o.strip() for o in env(
+        "CSRF_TRUSTED_ORIGINS",
+        default="https://vinihew105devlugcom.pythonanywhere.com"
+    ).split(",")
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -30,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.humanize',
-
+        
     "apps.users",
     "apps.customer",
     "apps.order",
@@ -135,11 +147,16 @@ if DEBUG:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AUTH_USER_MODEL = "user.User"
+AUTH_USER_MODEL = "users.User"
 
 
-CORS_ALLOWED_ORIGINS = [o.strip() for o in env("CORS_ALLOWED_ORIGINS").split(",")]
-CSRF_TRUSTED_ORIGINS = [o.strip() for o in env("CSRF_TRUSTED_ORIGINS").split(",")]
+# CORS_ALLOWED_ORIGINS = [o.strip() for o in env("CORS_ALLOWED_ORIGINS").split(",")]
+# CSRF_TRUSTED_ORIGINS = [o.strip() for o in env("CSRF_TRUSTED_ORIGINS").split(",")]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://vinihew105devlugcom.pythonanywhere.com"
+]
+
 
 if not DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
