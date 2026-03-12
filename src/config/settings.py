2aa6179213ab7eb604fd8a9738445d93a54403db
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-03-12 19:40:56 +0500

    yana hech narsa

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,7 +19,7 @@ DEBUG = bool(env("DEBUG"))
 
 import os
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.humanize',
-
+        
     "apps.users",
     "apps.customer",
     "apps.order",
@@ -144,7 +144,7 @@ if DEBUG:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "user.User"
+AUTH_USER_MODEL = "users.User"
 
 
 # CORS_ALLOWED_ORIGINS = [o.strip() for o in env("CORS_ALLOWED_ORIGINS").split(",")]
