421cd98ad153b240785b5a1a47c1ebf1e23ca3c3
muxammadsodiq <muxammadsodiq2703@gmail.com>
2026-03-12 21:06:13 +0500

    sal pal to'g'irlash

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,11 +19,8 @@ DEBUG = bool(env("DEBUG"))
 
 import os
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "vinihew105devlugcom.pythonanywhere.com",
-]
+ALLOWED_HOSTS = ['*']
+
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
